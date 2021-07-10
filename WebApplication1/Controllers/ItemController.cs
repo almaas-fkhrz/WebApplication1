c36a833ca9379540362c644af9c5c6f645df8832
Almaas <almaasfakhrurozi763@gmail.com>
2021-07-10 20:22:22 +0700

    Add project files.

---
+++ WebApplication1/Controllers/ItemController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
+        }
+    }
+}
---
+++ WebApplication1/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Borrower { get; set; }
+    }
+}
