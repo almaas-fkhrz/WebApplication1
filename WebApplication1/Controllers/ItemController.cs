603e2e4327d1066df3ca2c912320c0edf2323ec2
Almaas <almaasfakhrurozi763@gmail.com>
2021-07-14 20:29:38 +0700

    Add update function

--- WebApplication1/Controllers/ItemController.cs
+++ WebApplication1/Controllers/ItemController.cs
@@ -66,5 +66,33 @@ namespace WebApplication1.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+        //GET-Update
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Items.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        //POST-Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Item obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Items.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
     }
 }
